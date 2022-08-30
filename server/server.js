40a5bbab024e526ac892a5fd8846494d7be6b226
Michael Lam Cham Kee <mlamchamkee@gmail.com>
2022-08-30 12:03:36 -0400

    bare bones files

--- server/server.js
+++ server/server.js
@@ -4,12 +4,27 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/',
+  (req, res) => res.status(200).sendFile(path.join(__dirname, '../client/index.html'))
+)
+
+// Global error handling middleware
+app.use((err, req, res, next) => {
+    const defaultErr = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 400,
+      message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  });
 
 /**
  * start server
  */
  app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}`);
+    console.log(`Server listening on port: http://localhost:${PORT}`);
   });
   
   module.exports = app;
