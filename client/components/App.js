478212959ba69f235a9ff016b04454f94cf24a1c
Michael Lam Cham Kee <mlamchamkee@gmail.com>
2022-08-31 08:59:56 -0400

    wrote functionality for post request

--- client/components/App.js
+++ client/components/App.js
@@ -5,21 +5,43 @@ import Form from './Form.js';
 class App extends Component {
   constructor() {
     super();
-    this.state = {showModal: true};
+    this.state = { showModal: true };
     this.toggleModal = this.toggleModal.bind(this);
   }
 
   toggleModal() {
     console.log('Im running up that hill');
-    if (this.state.showModal) this.setState({showModal: false});
-    else this.setState({showModal: true});
+    if (this.state.showModal) this.setState({ showModal: false });
+    else this.setState({ showModal: true });
+  }
+
+  toggleModalSubmission() {
+    // modal for success message
+  }
+
+  // submits a post request to save profile on the database
+  postProfile() {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ 
+        fullName: document.querySelector('#full-name'),
+        // chosenName: document.querySelector('#preferred-name'),
+        gitHub: document.querySelector('#github'),
+      })
+    };
+
+    fetch('/bios/', requestOptions)
+      .then(response => response.json())
+      .then(() => this.toggleModal())
+      .catch(err => console.log('ERROR: Unable to create profile', err));
   }
 
   render() {
     return (
       <div>
-        <MainContainer id="main-container" toggleModal={this.toggleModal}/>
-        <Form id="modal" show={this.state.showModal}/>
+        <MainContainer id="main-container" toggleModal={ this.toggleModal }/>
+        {/* <Form id="modal" show={ this.state.showModal } postProfile={ this.postProfile }/> */}
       </div>
     );        
   }
--- client/components/Form.js
+++ client/components/Form.js
@@ -5,53 +5,53 @@ import { Delete as DeleteIcon } from '@mui/icons-material';
 
 class Form extends React.Component {
   render() {
-    if(this.props.show) {
-      // return (
-      //   <Dialog
-      //     open={this.props.show}
-      //     onClose={handleClose}
-      //     scroll={scroll}
-      //   >
-      //     <FormGroup className="form">
-      //       <Typography id="modal-modal-title" variant="h6" component="h2">
-      //           Create Codesmith Profile
-      //       </Typography>
-      //       <TextField label="Full Name" variant="outlined" />
-      //       <TextField label="Preferred Name" variant="outlined" />
-      //       <TextField label="GitHub" variant="outlined" />
-      //       <TextField label="Can be a resource for:" variant="outlined" />
-      //       <Button variant="contained" onClick={this.props.showModal} endIcon={<DeleteIcon/>}>
-      //         Cancel
-      //       </Button>
-      //       <Button variant="contained" onClick={this.props.showModal} endIcon={<SendIcon/>}>
-      //         Create
-      //       </Button>
-      //     </FormGroup>
-      //   </Dialog>
-      // );
-      return (
-        <Dialog
-          open={this.props.show}
-          onClose={this.props.showModal}
-          scroll="paper"
-        >
-          <DialogTitle id="scroll-dialog-title">Create Codesmith Profile</DialogTitle>
-          <DialogContent dividers={scroll === 'paper'}>
-            <TextField label="Full Name" variant="outlined" />
-            <TextField label="Preferred Name" variant="outlined" />
-            <TextField label="GitHub" variant="outlined" />
-            <TextField label="Can be a resource for:" variant="outlined" />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.props.showModal} endIcon={<DeleteIcon/>}>Cancel</Button>
-            <Button onClick={this.props.showModal} endIcon={<SendIcon/>}>Create</Button>
-          </DialogActions>
-        </Dialog>
+  // if(this.props.show) {
+    // return (
+    //   <Dialog
+    //     open={this.props.show}
+    //     onClose={handleClose}
+    //     scroll={scroll}
+    //   >
+    //     <FormGroup className="form">
+    //       <Typography id="modal-modal-title" variant="h6" component="h2">
+    //           Create Codesmith Profile
+    //       </Typography>
+    //       <TextField label="Full Name" variant="outlined" />
+    //       <TextField label="Preferred Name" variant="outlined" />
+    //       <TextField label="GitHub" variant="outlined" />
+    //       <TextField label="Can be a resource for:" variant="outlined" />
+    //       <Button variant="contained" onClick={this.props.showModal} endIcon={<DeleteIcon/>}>
+    //         Cancel
+    //       </Button>
+    //       <Button variant="contained" onClick={this.props.showModal} endIcon={<SendIcon/>}>
+    //         Create
+    //       </Button>
+    //     </FormGroup>
+    //   </Dialog>
+    // );
+    return (
+      <Dialog
+        open={this.props.show}
+        onClose={this.props.showModal}
+        scroll="paper"
+      >
+        <DialogTitle id="scroll-dialog-title">Create Codesmith Profile</DialogTitle>
+        <DialogContent dividers={scroll === 'paper'}>
+          <TextField id="full-name" label="Full Name" variant="outlined" />
+          {/* <TextField id="preferred-name" label="Preferred Name" variant="outlined" /> */}
+          <TextField id="github" label="GitHub" variant="outlined" />
+          {/* <TextField id="expertize" label="Can be a resource for:" variant="outlined" /> */}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.props.showModal} endIcon={<DeleteIcon/>}>Cancel</Button>
+          <Button onClick={this.props.postProfile} endIcon={<SendIcon/>}>Create</Button>
+        </DialogActions>
+      </Dialog>
 
-      );
-    }
+    );
   }
 }
+// }
 
 export default Form;
 
