49405ceabb7b485c0a99f2036c0c58e2c9be32ff
Michael Lam Cham Kee <mlamchamkee@gmail.com>
2022-09-01 10:07:26 -0400

    pictures loading from build assets

--- client/containers/AvatarContainer.jsx
+++ client/containers/AvatarContainer.jsx
@@ -17,11 +17,11 @@ class AvatarContainer extends React.Component {
         <Button id= { bio.gitHub } className="iris" onClick={ toggleBio }>
           <Avatar 
             alt={ bio.gitHub }
-            // src={ `client/assets/${bio.gitHub}.jpg` }
-            src="https://httpstatusdogs.com/img/300.jpg"
+            src={ `build/assets/${bio.gitHub}.jpg` }
+            // src={ 'build/assets/mlamchamkee.jpg' }
+            // src="https://httpstatusdogs.com/img/300.jpg"
             sx={ { width: 120, height: 120 } } 
           />
-          {/* onClick={ toggleBio } */}
         </Button>  
         <Button id= { bio.gitHub } className="name" onClick={ toggleBio }>
           { bio.preferredName }
