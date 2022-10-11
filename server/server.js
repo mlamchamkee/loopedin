eacf45999915495e8ad1f9219f05f4adc61ec4a8
Michael Lam Cham Kee <mlamchamkee@gmail.com>
2022-10-10 22:08:04 -0400

    dynamic port

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/',
   (req, res) => res.status(200).sendFile(path.join(__dirname, '../client/index.html')),
